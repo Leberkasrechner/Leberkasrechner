e7d6b97f9f402f7befac582962f4c43bebabd063
da-ssl <tassilo@thewalt.de>
2023-11-29 15:46:39 +0100

    map function

---
+++ components/util.php
@@ -0,0 +1,18 @@
+<?php
+function domap($lat, $lon, $text){
+    $lat = $lat + 0.05;
+    $ret = "<div style='height: 300px;' class='llmap' id='map'></div>";
+    $ret .= " <script>
+    var map = L.map('map').setView([$lat, $lon], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>-Mitwirkende'
+    }).addTo(map);
+    
+    L.marker([$lat, $lon]).addTo(map)
+        .bindPopup('$text')
+        .openPopup();
+    </script>
+    ";
+    return $ret;
+}
