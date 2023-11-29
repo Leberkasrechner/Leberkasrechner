a03317e8a64e42c0167f83f2090bc4ba28962a9f
da-ssl <tassilo@thewalt.de>
2023-11-29 17:52:59 +0100

    preparing for navbar search...

--- components/navbar.php
+++ components/navbar.php
@@ -35,18 +35,17 @@
       </div>
     </div>
     <div class="col-2 d-none d-xxl-block">
-                  <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
-                    <form action="./" method="get" autocomplete="off" novalidate="">
-                      <div class="input-icon">
-                        <span class="input-icon-addon">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path><path d="M21 21l-6 -6"></path></svg>
-                        </span>
-                        <form 
-                        <input type="text" value="" class="form-control" placeholder="Metzgerei suchen..." aria-label="Search in website">
-                      </div>
-                    </form>
-                  </div>
+        <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
+            <form role="search" action="/search.php" method="get" autocomplete="off" novalidate="">
+                <div class="input-icon">
+                    <span class="input-icon-addon">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path><path d="M21 21l-6 -6"></path></svg>
+                    </span>
+                    <input type="text" name="q" value="" class="form-control" placeholder="Metzgerei suchen..." aria-label="Metzgereien suchen">
                 </div>
+            </form>
+        </div>
+    </div>
   </div>
 </nav>
 <div class="container-xl">
