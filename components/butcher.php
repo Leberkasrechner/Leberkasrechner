5e8d23e3a0ad7ecc521af46e0c13fa0fa52f5ce8
da-ssl <tassilo@thewalt.de>
2023-11-30 21:22:20 +0100

    opening hours parser in php

--- components/butcher.php
+++ components/butcher.php
@@ -1,4 +1,7 @@
 <?php
+require "vendor/autoload.php";
+use Spatie\OpeningHours\OpeningHours;
+use Ujamii\OsmOpeningHours\OsmStringToOpeningHoursConverter;
 
 class Butcher {
     private $id;
@@ -47,6 +50,37 @@ class Butcher {
         } else {
             return $this->tags["name"];
         }
+    }
+
+    public function getOpeningStateHTML($colored=true) {
+        if(empty($this->tags["opening_hours"])||strlen($this->tags["opening_hours"]<3)) {return "";}
+        $ret = "";
+        try {
+            $opstr = preg_replace('/(,|;)\s+/', '$1', $this->tags["opening_hours"]); # Leerzeichen nach komma oder semikolon entfernen
+            $opstr = preg_replace('/\b(\d{1}:\d{2})\b/', '0$1', $opstr); # aus 6:30 mach 06:30
+
+            $openingHours = OsmStringToOpeningHoursConverter::openingHoursFromOsmString($opstr);
+            $now = new DateTime('now');
+            $range = $openingHours->currentOpenRange($now);
+        } catch (Exception $e) {
+            return $this->tags["opening_hours"];
+        }
+
+        if($range) {
+            $ret = "<span class='text-lime'>Geöffnet</span>";
+        } else {
+            $ret = "<span class='text-red'>Geschlossen</span>";
+        }
+
+        /*
+        if ($range) {
+            $ret .= "It's open since ".$range->start()."\n";
+            $ret .= "It will close at ".$range->end()."\n";
+        } else {
+            $ret .= "It's closed since ".$openingHours->previousClose($now)->format('l H:i')."\n";
+            $ret .= "It will re-open at ".$openingHours->nextOpen($now)->format('l H:i')."\n";
+        }*/
+        return $ret; 
     }
 
     public function getId() {
