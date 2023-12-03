8068d88144019feb7994c5d4a403467a6794514a
da-ssl <dassilo@thewalt.de>
2023-12-03 16:00:31 +0100

    contact page

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+    $page_title = "Kontakt";
+    require "components/head.php";
+    require "components/navbar.php";
+?>
+
+    <div class="page-header">
+        <h3>Kontakt</h3>
+    </div>
+    <div class="page-content">
+        <p>
+            E-Mail-Adresse:
+            <img src="static/mail.png" height="20" alt="E-Mail-Adresse" class>
+        </p>
+        <h3>Impressum</h3>
+        <p>Der Leberkasrechner ist ein privates Projekt und unterliegt deshalb nicht 
+            der Impressumspflicht laut Telemediengesetz § 5, Abs. 1</p>
+    </div>
+
+<?php
+    require "components/footer.php";
