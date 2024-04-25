11ac5dcddda783ed5824b3981218ad2701e1fa96
da-ssl <tassilo@thewalt.de>
2024-04-25 17:54:27 +0200

    new map service url also on butcher.php

--- components/util.php
+++ components/util.php
@@ -3,9 +3,9 @@ function domap($lat, $lon, $text){
     $lat = $lat;
     $ret = "<div style='height: 300px;' class='llmap' id='map'></div>";
     $ret .= " <script>
-    var map = L.map('map').setView([$lat, $lon], 13);
+    var map = L.map('map').setView([$lat, $lon], 11);
 
-    L.tileLayer('https://tiles.leberkasrechner.de/tile/{z}/{x}/{y}.png', {
+    L.tileLayer('https://tiles.leberkasrechner.de/styles/osm-bright/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>-Mitwirkende'
     }).addTo(map);
     
