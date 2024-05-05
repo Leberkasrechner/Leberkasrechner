d95b8783c8e066dff373cd29b1d82bd7b18f7cab
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2024-05-05 12:42:02 +0200

    create contribute.php
    This fixes #5

---
+++ contribute.php
@@ -0,0 +1,37 @@
+<?php
+    $page_title = "Mitmachen";
+    require "components/head.php";
+    require "components/navbar.php";
+?>
+
+<div class="page-header">
+<h1>Mitmachen</h1>
+</div>
+<div class="page-content">
+
+<p>Vielen Dank, dass Sie in Erwägung ziehen, mitzuwirken!</p>
+
+<p>Es gibt viele Möglichkeiten, um den Leberkasrechner zu verbessern.</p>
+
+<h2>Hinzufügen der Öffnungszeiten ihres örtlichen Metzgers</h2>
+
+<p> Dies ist eine großartige Möglichkeit, sowohl zu unserem Projekt als auch zur 
+OpenStreetMap-Gemeinschaft beizutragen. Wenn Sie neu bei OpenStreetMap sind, lesen Sie den
+<a href="https://learnosm.org/de">Leitfaden für Einsteiger</a></p>
+
+<h2>Fehler beheben oder Probleme melden</h2>
+
+<p>Wenn Sie einen Fehler in unserem Projekt finden, melden Sie ihn bitte, indem Sie einen neuen Eintrag in unserem GitHub-Repository erstellen. Wenn Sie neu auf GitHub sind, erfahren Sie <a href="https://docs.github.com/en/issues/tracking-your-work-with-issues/creating-an-issue">hier</a>, wie Sie einen Fehler melden können.
+
+</p>
+<h2>zum Code beitragen</h2>
+<p>Wenn Sie ein Entwickler sind, würden wir uns freuen, wenn Sie zu unserem Code beitragen. Sie können unser Repository forken, Ihre Änderungen vornehmen und dann einen Pull Request einreichen. Wenn Sie zum ersten Mal an Open-Source-Projekten auf GitHub mitarbeiten, lesen Sie <a href="https://opensource.guide/how-to-contribute/">diese Anleitung</a>.</p>
+<h2>Verbesserungsvorschläge und Vorschlag eines neuen Features</h2>
+
+<p>Falls Sie Ideen haben, wie der Leberkasrechner verbessert werden kann, oder Ideen für neue Feature haben, freuen wir uns, wenn Sie eine Diskussion <a href="https://github.com/Leberkasrechner/Leberkasrechner/discussions">hier</a> starten.</p>
+
+
+</div>
+<?php
+    require "components/footer.php";
+?>
